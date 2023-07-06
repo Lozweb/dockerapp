f15e73301f79f4fa286e8c9d3af52ec272a40398
BuildTools <aelozweb@gmail.com>
2023-07-06 23:32:12 +0200

    docker: work with docker compose

--- back/src/index.ts
+++ back/src/index.ts
@@ -26,15 +26,12 @@ app.use(compression());
 
 app.use(express.json());
 
-app.get("/install", async (req:Request, res:Response) => {
-
-    const table_install = fs.readFileSync(path.join(__dirname, "../../postgres/script/table.sql")).toString();
-
-    console.log(table_install);
-
+app.post("/install", async (req:Request, res:Response) => {
     try{
+        const table_install = fs.readFileSync(path.join(__dirname, "../script/table.sql")).toString();
+        console.log(table_install)
         await pool.query(table_install);
-        return res.status(200).json({message: "table install !"})
+        return res.status(200).json({message: "table install success!"})
     } catch (e) {
         return res.status(500).json({message: `error : ${e}`})
     }
@@ -42,6 +39,24 @@ app.get("/install", async (req:Request, res:Response) => {
 
 app.get("/", (req:Request, res:Response) => {
     return res.status(200).json({message: "i'm working !"})
+})
+
+app.post("/add", async (req:Request, res:Response) => {
+    console.log("call /add")
+    try {
+        console.log(req.body)
+        const elem = req.body.message as {message: string};
+        if(elem){
+            const q = "INSERT INTO docker (title) VALUES ($1)";
+            const insr = await pool.query(q, [elem])
+            if(insr.rowCount > 0) return res.status(200).json({message: "elem insert !"})
+            else return res.status(400).json({messaage: "Bad request ?"})
+        } else {
+            return res.status(400).json({message: "body is empty !"})
+        }
+    }catch (e){
+        return res.status(500).json({message: `error : ${e}`})
+    }
 })
 https.createServer(options, app).listen(config.API_PORT, () => {
     console.log(`Le serveur démarre sur le port ${config.API_PORT}`);
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,21 +6,29 @@ import './App.css';
 
 function App() {
 
-  const [backRep, setBackRep] = useState<string>()
+  const [backRep, setBackRep] = useState<string>();
+  const [install, setInstall] = useState<string>();
+  const [add, setAdd] = useState<string>();
 
   useEffect( () => {
-    const fetchData = async () => {
-      const rep = await fetch(
-          "https://localhost:5000",
-          {
-            method: "GET"
-          }
-      );
-      if(rep.status === 200) {
-        rep.body ? setBackRep((await rep.json()).message) : setBackRep("error");
-      }
+    const ping = async () => {
+      const rep = await fetch("https://localhost:5000", { method: "GET" });
+      if(rep.status === 200) rep.body ? setBackRep((await rep.json()).message) : setBackRep("error");
+      const install = await fetch("https://localhost:5000/install", { method: "POST" });
+      if(install.status === 200) install.body ? setInstall((await install.json()).message) : setInstall("error");
+      const add = await fetch("https://localhost:5000/add", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({
+          message: "add element from front"
+        })
+      });
+      add.body ? setAdd((await add.json()).message) : setAdd("error");
     }
-    fetchData().catch( error => console.log(error))
+    ping().catch( error => console.log(error))
+
   }, [])
 
   return (
@@ -39,6 +47,8 @@ function App() {
           Learn React
         </a>
         <p>{backRep}</p>
+        <p>installation table : {install}</p>
+        <p>Add : {add}</p>
       </header>
     </div>
   );
